b0d8f6e74d6f9d76ce269de3fdc9e769e1f721e1
jchou3 <jocelynchou@gmail.com>
2022-12-05 19:17:52 -0500

    finished filtering heatmap

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 def main():
     world = input("Enter the world file: ")
     path = input("Enter path file: ")
+    speed = input("How fast would you like to run the visualization (in seconds per step)? ")
+
+    s = float(speed)
 
     coords = []
     bot = []
@@ -27,7 +30,7 @@ def main():
     for line in coords:
         string = line.split()
         if string[2] != "B":
-            grid[(string[0], string[1])] = string[2]
+            grid[int(string[0]), int(string[1])] = string[2]
     
     f.close()
 
@@ -45,28 +48,43 @@ def main():
         truePath.append((cell[0], cell[1]))
 
     for step in range(101, 201):
-        move = bot[step]
+        move = bot[step].split()[0]
         moves.append(move)
     
     for step in range(201, 301):
-        observe = bot[step]
+        observe = bot[step].split()[0]
         evidence.append(observe)
 
     f.close()
     
     #Note: 100 cols x 50 rows, but input is in row, col order
     prediction = initialize(grid)
-    initial = np.zeros((50, 100))
-
+    initial = np.zeros((51, 101), dtype = float)
     data = tonumpy(prediction, initial)
     fig = plt.imshow(data, cmap = 'autumn', interpolation = 'none', aspect = 'equal')
-
+    plt.title("Prediction at t = {}".format(0))
+    plt.grid(color = "b", linestyle = "-", visible = True)
+    plt.draw()
+    plt.pause(s)
 
     for i in range(100):
         move = moves[i]
         observation = evidence[i]
 
+        prediction = filter(prediction, grid, move, observation) #gets dictionary of next step
+        data = tonumpy(prediction, initial) #puts info into numpy
 
+        fig.set_data(data)
+
+        spot = mostLikely(prediction)
+        if len(spot) <= 5:
+            print(spot)
+            
+        plt.title("Prediction at t = {}".format(i + 1))
+        plt.draw() #draws new heatmap
+        plt.pause(s)
+
+    plt.show()
 
 
 
@@ -79,4 +97,14 @@ def initialize(grid):
 
 
 def tonumpy(dict, array):
-    pass
+    for key, val in dict.items():
+        array[key[0]][key[1]] = val
+
+    return array
+
+
+def mostLikely(dict):
+    max_keys = [key for key, value in dict.items() if value == max(dict.values())]
+    return max_keys
+
+main()
